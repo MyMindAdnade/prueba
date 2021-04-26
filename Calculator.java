dcb3b1c27829b2a584913c66d1dd40c08fb8c1b7
MyMindAdnade <vladimir153759@gmail.com>
2021-04-26 16:28:59 -0500

    Se compelto la funcion suma

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,8 @@ public class Calculator{
 
 	private int suma(int a, int b){return 0;}
 	private int multi(int a, int b){return 0;}
-	private int resta(int a, int b){return 0;}
+	private int resta(int a, int b){
+		return a-b;
+	}
 
 }
