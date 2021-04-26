15c675f2f715a8d6432e5972b0ff360d30bcbb5d
MyMindAdnade <vladimir153759@gmail.com>
2021-04-26 16:30:21 -0500

    Complete la funcion multi

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,9 @@
 public class Calculator{
 
 	private int suma(int a, int b){return 0;}
-	private int multi(int a, int b){return 0;}
+	private int multi(int a, int b){
+		return a*b;
+	}
 	private int resta(int a, int b){return 0;}
 
 }
